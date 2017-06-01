9dddf2c8be9eaf0fb95281d813e3f31d2d9cb401
Sheng <yushenglin@guoshipartners.com>
2017-06-01 22:55:53 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <Slideshow
+      :sources="sources"
+      :names="names">
+    </Slideshow>
+  </div>
+</template>
+
+<script>
+import Slideshow from './vue-slideshow';
+
+export default {
+  name: 'app',
+  components: {
+    Slideshow,
+  },
+  data() {
+    return {
+      names: 'sss',
+      sources: [
+        // {
+        //   src: '../static/001.mp4',
+        //   type: 'video/mp4',
+        // },
+        {
+          src: '../static/0001.mp4',
+          type: 'video/mp4',
+          url: '',
+          title: '0001',
+        },
+        // {
+        //   src: '../static/0002.mp4',
+        //   type: 'video/mp4',
+        //   url: '',
+        //   title: '0002',
+        // },
+        // {
+        //   src: '../static/0003.mp4',
+        //   type: 'video/mp4',
+        //   url: '',
+        //   title: '0003',
+        // },
+        // {
+        //   src: 'https://media.w3.org/2010/05/sintel/trailer.mp4',
+        //   type: 'video/mp4',
+        // },
+        // {
+        //   src: '../static/001.mp4',
+        //   type: 'video/mp4',
+        // },
+        // {
+        //   src: '../static/001.mp4',
+        //   type: 'video/mp4',
+        // },
+      ],
+    };
+  },
+};
+</script>
