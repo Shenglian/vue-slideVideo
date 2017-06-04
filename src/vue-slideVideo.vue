44e7ce8437a486aadceb8b132e4edfa758b26b62
Sheng <yushenglin@guoshipartners.com>
2017-06-04 16:48:36 +0800

    update

--- src/vue-slideVideo.vue
+++ src/vue-slideVideo.vue
@@ -150,7 +150,7 @@ $arrowsZIndex: 4;
     left: 0;
     right: 0;
     bottom: 0;
-    box-shadow: 0 0 100px 70px rgba(0, 0, 0, 1) inset;
+    box-shadow: 0 -20px 20px 20px rgba(0, 0, 0, 1) inset;
   }
 }
 
